7c8eeb17a1f64628d4e41f426cdcd1299e6348b4
VPatole <VPatole@INDLAPTOP0269.cis.cisintranet.com>
2023-08-17 21:23:09 +0530

    Return int type assignment

---
+++ src/testing/ReturnTypeAsgnmt.java
@@ -0,0 +1,14 @@
+package testing;
+
+public class ReturnTypeAsgnmt {
+
+	static int sum(int i , int j) {
+		int value = i+j;
+		return value;
+	}
+	
+	public int div(int x , int y) {
+		int divValue = x-y;
+		return divValue;
+	}
+}
