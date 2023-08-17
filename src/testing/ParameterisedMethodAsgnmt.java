64b3accddf71a507c3fba9333ab84f26c8a0cae3
VPatole <VPatole@INDLAPTOP0269.cis.cisintranet.com>
2023-08-17 21:13:58 +0530

    Parameterized Method Assignment

---
+++ src/testing/ParameterisedMethodAsgnmt.java
@@ -0,0 +1,15 @@
+package testing;
+
+public class ParameterisedMethodAsgnmt {
+
+	static int sum(int i , int j) {
+		int value = i+j;
+		return value;
+	}
+	
+	float div(float i , float j) {
+		float divValue = i-j;
+		return divValue;
+	}
+}
+
